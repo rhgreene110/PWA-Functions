d93363dbd36a2dc9b7cd038313f2e4077afef07b
Richard Greene <rhgreene110@mac.com>
2019-05-30 19:59:31 -0500

    new version with ADMIN account

--- config/database.js
+++ config/database.js
@@ -3,8 +3,10 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Set up default mongoose connection
-var mongoDBurl = 'mongodb://jerytest:IZ8nYAVROPoQL8kSalbtMwsAhBK8oKft7IdNMm6NFXHrPwinNVmxrGsmURfSD8N5yxFemsMRtIxX5DWG6epJ1A==@jerytest.documents.azure.com:10255/React?ssl=true&replicaSet=globaldb';
-mongoose.connect(mongoDBurl, { useNewUrlParser: true });
+var mongoDBurl = 'mongodb://rhg-pwa-db:suCMnGWwwm3Ykg5LD8VzBESIrk9mAKNJ0bVuIvDSXrFWpWrPIENPmwfp5RZyTHt5XxG26uwhzDlJpdc8fFuOmA%3D%3D@rhg-pwa-db.documents.azure.com:10255/RHG-PWA-mongodb-collection?ssl=true';
+mongoose.connect(mongoDBurl, {
+    useNewUrlParser: true
+});
 
 //Get the default connection
 var db = mongoose.connection;
@@ -18,34 +20,34 @@ db.once('open', (callback) => {
 
 //create schema
 const heroSchema = new Schema({
-        name:String,
-        message:String,
-        first_name: String,
-        middle_initial: String,
-        last_name: String,
-        street_address: String,
-        city_name: String,
-        state_name: String,
-        country_name: String,
-        day_of_birth: Date,
-        driver_license_number: String,
-        driver_license_state: String,
-        payment_card: String,
-        payment_card_id_number: String,
-        payment_card_expiration_date: Date,
+    name: String,
+    message: String,
+    first_name: String,
+    middle_initial: String,
+    last_name: String,
+    street_address: String,
+    city_name: String,
+    state_name: String,
+    country_name: String,
+    day_of_birth: Date,
+    driver_license_number: String,
+    driver_license_state: String,
+    payment_card: String,
+    payment_card_id_number: String,
+    payment_card_expiration_date: Date,
 
-        username: {
-            type: String,
-            required: true,  
-        },
-        password: {
-            type: String,
-            required: true,  
-        },
-        email: String,
+    username: {
+        type: String,
+        required: true,
     },
-    { timestamps: true }
-);
+    password: {
+        type: String,
+        required: true,
+    },
+    email: String,
+}, {
+    timestamps: true
+});
 
 //create model
 const Hero = mongoose.model('Hero', heroSchema);
