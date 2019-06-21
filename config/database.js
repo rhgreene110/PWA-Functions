e0be3672765e5bab01b04567d513e9c75adf4f2d
Richard Greene <rhgreene110@mac.com>
2019-06-20 20:15:19 -0500

    Thursday Jun 20 2019

--- config/database.js
+++ config/database.js
@@ -1,12 +1,11 @@
 //Import the mongoose module
 var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+require('dotenv').config();
 
 //Set up default mongoose connection
-var mongoDBurl = 'mongodb://rhg-pwa-db:suCMnGWwwm3Ykg5LD8VzBESIrk9mAKNJ0bVuIvDSXrFWpWrPIENPmwfp5RZyTHt5XxG26uwhzDlJpdc8fFuOmA%3D%3D@rhg-pwa-db.documents.azure.com:10255/RHG-PWA-mongodb-collection?ssl=true';
-mongoose.connect(mongoDBurl, {
-    useNewUrlParser: true
-});
+var mongoDBurl = process.env.MONGODB_CONNECTION;
+mongoose.connect(mongoDBurl, { useNewUrlParser: true });
 
 //Get the default connection
 var db = mongoose.connection;
@@ -20,34 +19,41 @@ db.once('open', (callback) => {
 
 //create schema
 const heroSchema = new Schema({
-    name: String,
-    message: String,
-    first_name: String,
-    middle_initial: String,
-    last_name: String,
-    street_address: String,
-    city_name: String,
-    state_name: String,
-    country_name: String,
-    day_of_birth: Date,
-    driver_license_number: String,
-    driver_license_state: String,
-    payment_card: String,
-    payment_card_id_number: String,
-    payment_card_expiration_date: Date,
+        name:String,
+        message:String,
+        first_name: String,
+        middle_initial: String,
+        last_name: String,
+        street_address: String,
+        city_name: String,
+        state_name: String,
+        country_name: String,
+        day_of_birth: Date,
+        driver_license_number: String,
+        driver_license_state: String,
+        payment_card: String,
+        payment_card_id_number: String,
+        payment_card_expiration_date: Date,
 
-    username: {
-        type: String,
-        required: true,
+        username: {
+            type: String,
+            // required: true,
+        },
+        password: {
+            type: String,
+            required: true,  
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        emailValid: {
+            type: Boolean,
+            default: false
+        },
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    email: String,
-}, {
-    timestamps: true
-});
+    { timestamps: true }
+);
 
 //create model
 const Hero = mongoose.model('Hero', heroSchema);
